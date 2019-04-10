f930b536e6ba06a86c0808ab8b4f636ad7cd6e76
Diego Pennisi <diegopennisi@outlook.es>
2019-04-10 15:22:24 +0200

    titulos editados con foto

--- pages/headers/titulos.php
+++ pages/headers/titulos.php
@@ -18,12 +18,61 @@
           <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
+                  <div class="card-header">
+                    Títulos de la imagen principal.
+                 </div>
+                 <div >
+                   <iframe class="iframe" width="100%" frameborder="no" Scrolling="no" height="700" src="http://localhost/webFran/"></iframe>
+                 </div>
+                 <hr>
+                 <form  method="post">
+                 <div class="row">
+                   <div class="col-md-4">
+                     <div class="form-group row">
+                       <div class="col-sm-12">
+                         <input type="text" class="form-control" name="primero" required value="<?php echo $respuesta['primero'] ?>" />
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-md-4">
+                     <div class="form-group row">
+                       <div class="col-sm-12">
+                         <input type="text" class="form-control" name="segundo" required value="<?php echo $respuesta['segundo'] ?>" />
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-md-4">
+                     <div class="form-group row">
+                       <div class="col-sm-12">
+                         <input type="text" class="form-control" name="tercero" required value="<?php echo $respuesta['tercero'] ?>" />
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-md-5"></div>
+                   <div class="col-md-4">
+                     <button type="submit" name="cambiar" class="btn btn-outline-primary">Cambiar</button>
+                   </div>
+                 </div>
+               </form>
               </div>
             </div>
           </div>
         </div>
+
+        <!-- Flexbox container for aligning the toasts -->
+          <div id="toast"><div id="img"><i class="far fa-laugh-beam"></i></div><div id="desc">Cambios Realizados...</div></div>
         <!-- content-wrapper ends -->
         <!-- partial:../../partials/_footer.html -->
+
+        <?php if (isset($_GET['id'])): ?>
+           <?php if ($_GET['id']== 'ok'): ?>
+              <script>
+              var x = document.getElementById("toast")
+               x.className = "show";
+                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+              </script>
+           <?php endif; ?>
+        <?php endif; ?>
 
         <?php include '../../head/footer.php'; ?>
         <!-- partial -->
--- php/headers/titulos.php
+++ php/headers/titulos.php
@@ -3,5 +3,33 @@
      header('location:php/login/login.php');
   }
 
+  require '../conexion.php';
+
+
+  $sql= $conexion->prepare("SELECT * FROM header");
+  $sql->execute();
+  $respuesta = $sql->fetch();
+
+
+  if (isset($_POST['cambiar'])) {
+    $primero = $_POST['primero'];
+    $segundo = $_POST['segundo'];
+    $tercero = $_POST['tercero'];
+     $sql=$conexion->prepare("UPDATE header SET primero = :primero , segundo = :segundo, tercero = :tercero");
+    if( $sql->execute(array(
+             ':primero'=>$primero,
+             ':segundo'=>$segundo,
+             ':tercero'=>$tercero
+     ))){
+       $sql= $conexion->prepare("SELECT * FROM header");
+       $sql->execute();
+       $respuesta = $sql->fetch();
+      header('location:../../php/headers/titulos.php?id=ok');
+     }else{
+       header('location:../../php/headers/titulos.php?id=error');
+     }
+  }
+
+
 require '../../pages/headers/titulos.php';
    ?>
