9b62d3e2985b88688b3bddfdb61deb630769d07b
Diego Pennisi <diegopennisi@outlook.es>
2019-04-12 20:38:43 +0200

    taemos los productos de la base de datos

---
+++ pages/pagina2/pagina2.php
@@ -0,0 +1,105 @@
+
+ <?php require '../../head/head.php'; ?>
+
+ <body>
+   <div class="container-scroller">
+     <!-- partial:partials/_navbar.html -->
+     <?php require '../../head/menu.php'; ?>
+  <!-- -->
+     <div class="container-fluid page-body-wrapper">
+       <!-- insertamos el sadibar -->
+       <?php require '../../head/sidebar.php'; ?>
+       <!-- fin de sidebar -->
+
+       <div class="main-panel">
+         <div class="content-wrapper">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-block">
+                    <ol class="breadcrumb alert alert-info">
+                       <li class="breadcrumb-item active">Página de <?php echo $res['segundo'] ?>.</li>
+                    </ol>
+                  </div>
+                  <hr>
+         <form class=""  method="post">
+
+           <div class="row">
+             <div class="col-md-4"></div>
+             <div class="col-md-8">
+               <h1><?php echo $res['segundo']; ?></h1>
+             </div>
+             <div class="col-md-3"></div>
+             <div class="col-md-4">
+               <input type="text" class="form-control" name="subtitulo" value="<?php echo $pagi2['subpagina'] ?>">
+             </div>
+
+           </div>
+           <br><br>
+           <div class="row text-center container">
+               <div class="col-md-4">
+                   <span class="fa-stack fa-4x">
+           <i class="fas fa-circle fa-stack-2x text-warning"></i>
+           <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+         </span>
+                <input required class="form-control" type="text" name="titulo1" value=" <?php echo $pagi1['titulo1'] ?>">
+                 <br>
+                <input required class="form-control" type="text" name="desc1" value=" <?php echo $pagi1['desc1'] ?>">
+
+               </div>
+               <div class="col-md-4">
+                   <span class="fa-stack fa-4x">
+           <i class="fas fa-circle fa-stack-2x text-warning"></i>
+           <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+         </span>
+         <input required class="form-control" type="text" name="titulo2" value=" <?php echo $pagi1['titulo2'] ?>">
+          <br>
+         <input required class="form-control" type="text" name="desc2" value=" <?php echo $pagi1['desc2'] ?>">
+               </div>
+               <div class="col-md-4">
+                   <span class="fa-stack fa-4x">
+           <i class="fas fa-circle fa-stack-2x text-warning"></i>
+           <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+         </span>
+         <input required class="form-control" type="text" name="titulo3" value=" <?php echo $pagi1['titulo3'] ?>">
+          <br>
+         <input required class="form-control" type="text" name="desc3" value=" <?php echo $pagi1['desc3'] ?>">
+               </div>
+           </div>
+           <br><br>
+           <div class="row">
+             <div class="col-md-12">
+               <button type="submit" class="btn btn-outline-primary btn-block" name="pagina1">Editar cambios</button>
+             </div>
+             <br><br>
+           </div>
+         </form>
+
+                </div>
+
+              </div>
+            </div>
+         </div>
+         <div id="toast"><div id="img"><i class="far fa-laugh-beam"></i></div><div id="desc">Cambios Guardados!!</div></div>
+         <?php if (isset($_GET['id'])): ?>
+            <?php if ($_GET['id']== 'ok'): ?>
+               <script>
+               var x = document.getElementById("toast")
+                x.className = "show";
+                 setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+               </script>
+            <?php endif; ?>
+         <?php endif; ?>
+         <!-- content-wrapper ends -->
+         <!-- partial:../../partials/_footer.html -->
+
+         <?php include '../../head/footer.php'; ?>
+         <!-- partial -->
+       </div>
+
+           </div>
+         </div>
+         <!-- content-wrapper ends -->
+ </body>
+
+ </html>
---
+++ php/pagina2/pagina2.php
@@ -0,0 +1,52 @@
+<?php session_start();
+
+  if (!isset($_SESSION['userName'])) {
+    header('location:../login/login.php');
+  }
+
+
+require '../conexion.php';
+
+$sql = $conexion->prepare('SELECT * FROM menu');
+$sql->execute();
+$res=$sql->fetch();
+
+$sql1 = $conexion->prepare('SELECT * FROM pagina2');
+$sql1->execute();
+$pagi2=$sql1->fetch();
+
+
+if (isset($_POST['pagina1'])) {
+  $subtitulo = $_POST['subtitulo'];
+  $titulo1 = $_POST['titulo1'];
+  $titulo2 = $_POST['titulo2'];
+  $titulo3 = $_POST['titulo3'];
+  $desc1 = $_POST['desc1'];
+  $desc2 = $_POST['desc2'];
+  $desc3 = $_POST['desc3'];
+
+  $sql=$conexion->prepare("UPDATE pagina1 SET subtitulo = :subtitulo , titulo1=:titulo1, titulo2 = :titulo2, titulo3 = :titulo3, desc1= :desc1, desc2 = :desc2, desc3 = :desc3");
+  if ($sql->execute(array(
+     ':subtitulo'=>$subtitulo,
+     ':titulo1'=>$titulo1,
+     ':titulo2'=>$titulo2,
+     ':titulo3'=>$titulo3,
+     ':desc1'=>$desc1,
+     ':desc2'=>$desc2,
+     ':desc3'=>$desc3,
+  ))) {
+    header('location:../../php/pagina1/pagina2.php?id=ok');
+  }else{
+    header('location:../../php/pagina1/pagina2.php?id=error');
+  }
+}
+
+
+
+
+
+  require '../../pages/pagina2/pagina2.php';
+
+
+
+ ?>
